43990a96dd43654a0275eeaa090ce178d8d19441
fkochanowski <meetspamhere+git@gmail.com>
2022-01-07 18:55:50 +0100

    update

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import markerList from '/markerList.js';
+
 //fullscreen control
-import {FullScreen, defaults as defaultControls} from 'ol/control';
+import {FullScreen, defaults as defaultControls, ZoomSlider, MousePosition} from 'ol/control';
 
 //points
 import Feature from 'ol/Feature';
@@ -15,29 +16,16 @@ import VectorSource from 'ol/source/Vector';
 import {Icon, Style} from 'ol/style';
 import {Vector as VectorLayer} from 'ol/layer';
 import { fromLonLat } from 'ol/proj';
+import {createStringXY} from 'ol/coordinate';
 
-//var fs = require('fs');
-
-// let iconFeature1 = new Feature({
-//   geometry: new Point(fromLonLat([ markerList.locations[0].lon, markerList.locations[0].lat ])),
-//   name: markerList.locations[0].name,
-//   description: markerList.locations[0].description,
-//   type: markerList.locations[0].type
-// });
-
-// const iconFeature2 = new Feature({
-//   geometry: new Point(fromLonLat([20, 10])),
-//   name: 'Null Island2',
-//   description: 'Island full of nulls222',
-//   type: 'loco'
-// });
-
-// const iconFeature3 = new Feature({
-//   geometry: new Point(fromLonLat([ markerList['locations'][1]['lon'], markerList['locations'][1]['lat'] ])),
-//   name: 'Null Island2',
-//   description: 'Island full of nulls222',
-//   type: 'loco'
-// });
+const mousePositionControl = new MousePosition({
+  coordinateFormat: createStringXY(4),
+  projection: 'EPSG:4326',
+  // comment the following two lines to have the mouse position
+  // be placed within the map.
+  className: 'custom-mouse-position',
+  target: document.getElementById('mouse-position'),
+});
 
 const bistroStyle = new Style({
   image: new Icon({
@@ -55,6 +43,7 @@ const locoStyle = new Style({
     src: 'data/iconLoco.png',
   }),
 });
+
 const locationsNum = Object.keys(markerList.locations).length;
 let featuresList = [];
 for(let i = 0; i < locationsNum; i++) {
@@ -65,7 +54,6 @@ for(let i = 0; i < locationsNum; i++) {
     type: markerList.locations[i].type
   }))
 }
-//const featuresList = [iconFeature1, iconFeature2, iconFeature3];
 
 for(let i = 0; i < featuresList.length; i++){
   if(featuresList[i].get('type') === 'gastro'){
@@ -83,18 +71,23 @@ const vectorLayer = new VectorLayer({
 });
 
 const map = new Map({
-  controls: defaultControls().extend([new FullScreen()]),
+  controls: defaultControls().extend([new FullScreen()]).extend([mousePositionControl]),
   layers: [ new TileLayer({
     source: new OSM()
   }), vectorLayer
   ],
   target: document.getElementById('map'),
   view: new View({
-    center: [0, 0],
-    zoom: 2
+    center: fromLonLat([18.6124, 54.3717]),
+    zoom: 18,
+    minZoom: 15,
+    maxZoom: 20,
   })
 
 });
+
+const zoomslider = new ZoomSlider();
+map.addControl(zoomslider);
 
 //point cd
 const element = document.getElementById('popup');
@@ -119,29 +112,21 @@ map.on('click', function (evt) {
   const feature = map.forEachFeatureAtPixel(evt.pixel, function (feature) {
     return feature;
   });
-  //const featureContents = `${feature.get('name')} ${feature.get('description')}`;
+
   if (feature) {
     popup.setPosition(evt.coordinate);
     content.innerHTML = '<p>Nazwa : ' + feature.get('name') + '</p>' 
       + '<p>Opis : ' + feature.get('description') + '</p>'
-    /*$(element).popover({
-      placement: 'top',
-      html: true,
-      content: featureContents
-    });
-    $(element).popover('show');
-  } else {
-    $(element).popover('dispose');*/
+
   }
 });
 
-// change mouse cursor when over marker
 map.on('pointermove', function (e) {
   const pixel = map.getEventPixel(e.originalEvent);
   const hit = map.hasFeatureAtPixel(pixel);
   map.getTarget().style.cursor = hit ? 'pointer' : '';
 });
-// Close the popup when the map is moved
+
 map.on('movestart', function () {
   $(element).popover('dispose');
 });
--- markerList.js
+++ markerList.js
@@ -16,18 +16,25 @@ export default
                 "type": "loco"
             },
             {
-                "name": "C2",
-                "lon": "20",
-                "lat": "20",
-                "description": "B2",
+                "name": "BUS Siedlicka",
+                "lon": "18.61255",
+                "lat": "54.3701",
+                "description": "Przystanek autobusowy Siedlicka, kierunek Wrzeszcz PKP",
                 "type": "loco"
             },
             {
-                "name": "D2",
-                "lon": "10",
-                "lat": "10",
-                "description": "B2",
+                "name": "Kambuz",
+                "lon": "18.61335",
+                "lat": "54.37283",
+                "description": "Więcej jedzenia",
                 "type": "gastro"
-            }
+            },
+            {
+              "name": "Pasibrzuch",
+              "lon": "18.61205",
+              "lat": "54.37165",
+              "description": "Śniadania, obiady i przekąski",
+              "type": "gastro"
+          }
     ]
 }
